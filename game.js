bbaca0fe0343cf83f7e9c7f0dc60f9576f76a30d
David Martin <demartins37@yahoo.com>
2019-07-12 19:20:31 -0400

    updated js and a litlle bootstrap

--- game.js
+++ game.js
@@ -1,10 +1,13 @@
-var sports= ["Football", "Baseball", "Basketball", "Soccer", "Golf"];
+
+var sports= ["Football", "Baseball", "Basketball", "Soccer", "Hockey", "Golf", "Tennis"];
 
 function makeButtons() {
   $("#buttons-view").empty();
   for (let i = 0; i < sports.length; i++) {
     var buttonBuilder = $("<button>");
     buttonBuilder.addClass("sportsBtn");
+    buttonBuilder.addClass("btn-sm");
+    buttonBuilder.addClass("btn-primary");
     buttonBuilder.attr("data-sport", sports[i]);
     buttonBuilder.text(sports[i]);
     $("#buttons-view").append(buttonBuilder);
@@ -18,7 +21,7 @@ $("#add-sports").click(function(e){
 });
 
 function giphySearch() {
-  $("#sports-images").empty();
+
   var apiKey = "api_key=BNL3mZRIw0qoOhOBwua4UPr3wEBFagsz";
   var sportsClick = $(this).attr("data-sport");
   var queryUrl = "https://api.giphy.com/v1/gifs/search?" + apiKey + "&q=" + sportsClick + "&limit=10&rating=pg"
@@ -29,17 +32,31 @@ function giphySearch() {
   }).then(function(response){
     console.log(response);
     var arrayOfGifs= response.data
+    
+    $("#sports").empty();
     for (let j = 0; j < arrayOfGifs.length; j++) {
       var currentGif = arrayOfGifs[j];
       console.log(arrayOfGifs[j].images.fixed_height_still.url);
-      var imageHolder = $("<div class= 'gif'>");
+      var imageHolder = $("<div class= 'ml-4 float-left gif'>");
+      
       var gifRating = currentGif.rating;
       var p = $("<p>").text("Rating: "+ gifRating);
       imageHolder.append(p);
-      var imageUrl = currentGif.images.fixed_height_still.url;
-      var sportsImage = $("<img>").attr("src", imageUrl);
+      var animate = currentGif.images.fixed_height.url;
+      var still = currentGif.images.fixed_height_still.url;
+      var sportsImage = $("<img>");
+      sportsImage.addClass("sports-image");
+      sportsImage.attr("src", still);
+      sportsImage.attr("data-still", still);
+      sportsImage.attr("data-animate", animate);
+      sportsImage.attr("data-state", "still");
       imageHolder.append(sportsImage);
-      $("#sports-images").prepend(imageHolder);
+      $("#sports").append(imageHolder);
+      
+      
+      
+      
+      
       
     };
   });
@@ -49,19 +66,16 @@ $(document).on("click", ".sportsBtn", giphySearch);
 
 
 
-$(".gif").on("click", function() {
-  console.log(this);
-
-  var state = $(this).attr("data-state");
-  var animatedUrl = $(this).attr("data-animate");
-  var stillUrl = $(this).attr("data-still");
-
-  if (state === "still") {
-    $(this).attr("src", animatedUrl);
+$(document).on("click", ".sports-image", function(){
+  if ($(this).attr("data-state") === "still") {
+    console.log("was still")
+    $(this).attr("src", $(this).attr("data-animate"));
     $(this).attr("data-state", "animate");
   }
   else {
-    $(this).attr("src", stillUrl);
+    console.log("was animate")
+    $(this).attr("src", $(this).attr("data-still"));
     $(this).attr("data-state", "still");
-  }
+}
 });
+
